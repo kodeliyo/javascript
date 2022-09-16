785defa7725801ab13dd62f3272b8aea5fba607b
kodeliyo <kodeliyo>
2022-09-16 18:21:37 +0200

    search elements in array

---
+++ search/index.js
@@ -0,0 +1,13 @@
+
+const arr = ["Ana", "John", "Julio", "Alejandro"];
+
+console.log(arr.includes("Ana"));
+console.log(arr.includes("Pedro"));
+
+console.log(arr.includes("Julio", 2));
+
+console.log(arr.indexOf("Alejandro"));
+console.log(arr.indexOf("Julio", 2));
+
+console.log(arr.lastIndexOf("Alejandro"));
+console.log(arr.lastIndexOf("Julio", 2));
